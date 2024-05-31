2316bcacb4ee5a3af0a053f4f6afcbac110244c8
Mariya Zakharova <zakharovamaria73@yandex.ru>
2024-05-31 22:32:22 +0400

    feat: added product block

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const infoBtns = document.querySelectorAll('.info-dot');
+const infoHints = document.querySelectorAll('.info-hint');
+
+for (let btn of infoBtns) {
+	btn.addEventListener('click', showHint);
+}
+
+function showHint(e) {
+	e.stopPropagation();
+	this.parentNode.querySelector('.info-hint').classList.toggle('none');
+}
+
+function closeHint() {
+	for (let hint of infoHints) {
+		hint.classList.add('none');
+	}
+}
+
+document.addEventListener('click', closeHint);
+
+// запрещаем всплытие события при клике на подсказки
+for (let hint of infoHints) {
+	hint.addEventListener('click', e => e.stopPropagation());
+}
